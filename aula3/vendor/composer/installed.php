4770c3754fdef9ee3a90508741bcc71cd9defb16
dogneo31-ctrl <dogneo31@gmail.com>
2026-08-20 14:09:42 -0300

    first commit

---
+++ aula3/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'projeto/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '48d64c5bad19c638e9b77fa557a6d65fe0f2adc0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'projeto/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '48d64c5bad19c638e9b77fa557a6d65fe0f2adc0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
