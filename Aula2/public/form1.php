35bfdc981d45c5b226f09ffc4f3e4f26d6c67d8f
dogneo31-ctrl <dogneo31@gmail.com>
2026-09-01 15:46:24 -0300

    first commit

--- Aula2/public/form1.php
+++ Aula2/public/form1.php
@@ -1,39 +1,47 @@
 <?php
-
-require "layout.php";
-echo "Recebido";
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $numero1 = $_POST["numero1"];
-    $numero2 = $_POST["numero2"];
-
-    echo "Número 1: " . $numero1 . "<br>";
-    echo "Número 2: " . $numero2;
-}
+    require "layout.php";
+   
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $numero1 = $_POST["numero1"];
+        $numero2 = $_POST["numero2"];
+   
+        echo "Número 1: " . $numero1 . "<br>";
+        echo "Número 2: " . $numero2;
+    }
 
 ?>
 
-<form method="POST" action="recebe.php">
-    <label for="num1">Número 1</label>
-    <input type="number" name="numero1" id="numero1" required autofocus>
-    <br><br>
-    <label for="num2">Número 2</label>
-    <input type="number" name="numero2" id="numero2" required>
-
-    <br><br>
-
-    <input type="submit"value="Post outra Página">
+-------------------------- POST no mesmo form
+<form method="POST">
+<label for="num1">Número 1</label>
+<input type="number" name="numero1" id="numero1" required >
+<br><br>
+<label for="num2">Número 2</label>
+<input type="number" name="numero2" id="numero2" required>
+<br><br>
+<input type="submit" class="btn btn-primary" value="Post Mesmo Form">
 </form>
 
+<hr>
+-------------- POST para outra página
+<form method="POST" action="recebe.php">
+<label for="num1">Número 1</label>
+<input type="number" name="numero1" id="numero1" required >
+<br><br>
+<label for="num2">Número 2</label>
+<input type="number" name="numero2" id="numero2" required>
+<br><br>
+<input type="submit"  class="btn btn-success" value="Post outra página">
+</form>
+<hr>
+--------------------- GET para outra página
 
-
-<form method="POST">
-    <label for="num1">Número 1</label>
-    <input type="number" name="numero1" id="numero1" required autofocus>
-    <br><br>
-    <label for="num2">Número 2</label>
-    <input type="number" name="numero2" id="numero2" required>
-
-    <br><br>
-
-    <input type="submit"value="Salvar">
+<form method="GET" action="recebe.php">
+<label for="num1">Número 1</label>
+<input type="number" name="numero1" id="numero1" required >
+<br><br>
+<label for="num2">Número 2</label>
+<input type="number" name="numero2" id="numero2" required>
+<br><br>
+<input type="submit" class="btn btn-danger" value="Post outra página">
 </form>
